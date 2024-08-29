8c63dd77f14072a9f6b13185f242eccdaa612b33
hungdinh1212 <dinhhung12122003@gmail.com>
2024-08-29 07:04:01 +0700

    update

---
+++ ThucHanhTuan01/DeveloperSkills&EditorSetup/Bai01.js
@@ -0,0 +1,12 @@
+const printForeCast=(arr)=>{
+    let forecastString="";
+    for(let i=0;i<arr.length;i++){
+        forecastString +=`...${arr[i]}*C in ${i+1} days`;
+        console.log(forecastString+"");
+    }
+
+}
+// Data 1
+printForeCast([17,21,23])
+// Data 2
+printForeCast([12,5,-5,0,4])
